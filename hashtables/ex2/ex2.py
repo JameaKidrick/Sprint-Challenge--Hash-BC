6983290cc0c89f9da64404115cf15c9dc1f09422
JameaKidrick <50430635+JameaKidrick@users.noreply.github.com>
2020-04-10 14:01:44 -0400

    almost finished hashtables ex2, just need to remove last None then finished

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -13,11 +13,33 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
+    """
+    output an array of strings with the entire route of your trip. For the above example, it should look like this:
+    ["LAX", "SFO", "BHM", "FLG", "XNA", "CID", "SLC", "PIT", "ORD"]
+    """
     hashtable = HashTable(length)
     route = [None] * length
+    # LOOP THROUGH TICKETS ARRAY
+    for i in tickets:
+        # INSERT SOURCE, DESTINATION INTO HASHTABLE
+        if i.source is not None:
+            hash_table_insert(hashtable, i.source, i.destination)
+            # HASHTABLE === [<(PIT, ORD)>, <(XNA, CID)>...]
+            # FIND NONE == SOURCE
+            # ADD DESTINATION TO ROUTE
+            # [LAX, NONE, NONE, NONE...]
+            if i.source == 'NONE':
+                route[0] = i.destination
+    # LOOP THROUGH ROUTE ARRAY
+    for travel in range(len(route)):
+    # KEY = ROUTE[I] // RETRIEVE VALUE
+        if route[travel] is not None:
+            next_route = hash_table_retrieve(hashtable, route[travel])
+            # print(next_route)
+    # PLACE VALUE IN NEXT ROUTE INDEX IF NOT NONE
+        if next_route is not 'NONE' and next_route is not None:
+            print(next_route)
+            route[travel + 1] = next_route
+    print(route)
+    return route
 
-    """
-    YOUR CODE HERE
-    """
-
-    pass
